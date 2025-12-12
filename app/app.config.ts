81b8cfe6f4ed75da5b087f32c313a81c50ef61ff
gj-wes <negativegary@gmail.com>
2025-12-12 13:59:54 +0000

    Adjust NuxtUI styling dark mode inputs

--- app/app.config.ts
+++ app/app.config.ts
@@ -4,6 +4,21 @@ export default defineAppConfig({
       primary: 'blue',
       neutral: 'slate'
     },
+    input: {
+      slots: {
+        base: 'dark:bg-neutral-700'
+      }
+    },
+    textarea: {
+      slots: {
+        base: 'dark:bg-neutral-700'
+      }
+    },
+    select: {
+      slots: {
+        base: 'dark:bg-neutral-700'
+      }
+    },
     button: {
       slots: {
         base: "rounded px-4 py-2 text-center uppercase leading-8 border-none cursor-pointer justify-center disabled:[--tw-bg-opacity:0.8] font-normal"
